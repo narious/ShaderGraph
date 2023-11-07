6545a20f310d09ad8c8f37d3443a3f749e8af623
Michael Dinh <tonkoalsce@gmail.com>
2023-11-07 19:51:09 +1100

    1-CheckerPattern Unity Fin

---
+++ ShaderGraph/Assets/Scripts/CameraRoatateAxis.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRoatateAxis : MonoBehaviour
+{
+    [SerializeField] private GameObject pivot;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
